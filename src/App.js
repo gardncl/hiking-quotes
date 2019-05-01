b095497242cb5720cb92cb090174e2135b2854d8
gardncl <gardncl@gmail.com>
2019-04-30 20:50:03 -0400

    quote

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>"To be honest you guy don't smell as bad as I thought you would"</p>
       </header>
     </div>
   );
