d6c449d4ee865576f0306a33a051b8765f5fe367
Clay Gardner <gardncl@gmail.com>
2019-05-03 21:16:52 -0400

    Add helper method to generate quotes (#1)
    # Why 
    There is going to be a long list of quotes to choose from and a method for choosing them so this won't fit in the body of the HTML. The quote generator should be split out separately. 
    
    # What
     - [x] New method to return a string that's a quote
     - [x] Render this quote in the main application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import randomQuote from './randomQuote.ts';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>"To be honest you guy don't smell as bad as I thought you would"</p>
+        <p>{randomQuote()}</p>
       </header>
     </div>
   );
---
+++ src/randomQuote.ts
@@ -0,0 +1,5 @@
+const randomQuote = (): string => {
+    return "To be honest you guy don't smell as bad as I thought you would"
+};
+
+export default randomQuote;
