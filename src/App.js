a3efa8df7fdf7638349e855042f56879f8041fc1
Clay Gardner <gardncl@gmail.com>
2019-05-04 21:20:53 -0400

    Add more quotes and produce random one on load (#4)
    # Why
    We need more quotes
    
    # What
     - [x] Random quote now displays

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>{randomQuote()}</p>
+        <p>"{randomQuote()}"</p>
       </header>
     </div>
   );
